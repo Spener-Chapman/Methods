98ecc0217e95cba696740d55b8d2f3d2ce9204a4
Spener-Chapman <spencerchap@hotmail.com>
2021-09-27 12:41:38 -0300

    showcased a showhud method

--- Methods/Program.cs
+++ Methods/Program.cs
@@ -5,11 +5,53 @@ using System.Text;
 using System.Threading.Tasks;
 
 namespace Methods
-{
+{ // declarations
+    
     class Program
     {
+        static int score;
+        static string name;
+        static int xp;
+        static int gold;
+        // this method should be done down 
+        static void ShowHUD() // method that shows the hud
+        {
+            Console.WriteLine("score =" + score);
+            Console.WriteLine("name = " + name);
+            Console.WriteLine("xp = " + xp);
+            Console.WriteLine("gold = " + gold);
+            Console.WriteLine();
+        }
         static void Main(string[] args)
         {
+            // Methods: 
+            // (AKA Procedures, Functions, etc.)
+            Console.WriteLine("Methods:");
+            
+
+            //...
+
+            // initialize
+            score = 0;
+            name = "Musu";
+            xp = 0;
+            gold = 0;
+
+            // hud
+            // THIS HUD IS BAD, only for demontration
+            Console.WriteLine();
+            ShowHUD();
+
+            // simulated gameplay
+            score = score + 100;
+            xp = xp + 50;
+
+            ShowHUD();
+            score = score + 1000;
+            xp = xp + 520;
+            ShowHUD();
+
+            Console.ReadKey(true);
         }
     }
 }
